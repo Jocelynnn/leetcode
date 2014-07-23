93629dee31620a80b5a908a90cb8149c096bf9dd
Jocelynnn <angle199407@163.com>
2014-07-23 11:54:26 +0800

    test

---
+++ main/main/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  main
+//
+//  Created by 蒋秉洁 on 7/23/14.
+//  Copyright (c) 2014 蒋秉洁. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
